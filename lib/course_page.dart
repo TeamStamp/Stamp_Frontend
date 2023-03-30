5c32fdf89ee582da776f26fdf6fe73d88de32d65
201904027 <scottmctominay39@naver.com>
2023-03-30 16:20:41 +0900

    course_page 생성

--- lib/course_page.dart
+++ lib/course_page.dart
@@ -8,18 +8,45 @@
 
  */
 
-
 import 'package:flutter/material.dart';
 
-class coursepage extends StatelessWidget {
+class coursepage extends StatefulWidget {
   const coursepage({Key? key}) : super(key: key);
 
   @override
+  State<coursepage> createState() => _coursepageState();
+}
+
+class _coursepageState extends State<coursepage> {
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
-          child: Text('CoursePage')
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(MediaQuery.of(context).size.height/12),
+        child: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Color(0xffF8BD53),
+          elevation: 5,
+          centerTitle: true,
+          title: Container(
+            width: double.infinity,
+            height: MediaQuery.of(context).size.height/20,
+            color: Color(0xffF8BD53),
+            child: Image(
+              image: AssetImage('images/logo_appbar.png'),
+              width: double.infinity,
+              height: MediaQuery.of(context).size.height/20,
+            ),
+          ),
+        ),
+      ),
+      body: Column(
+        children: [
+          Flexible(child: Container(color: Colors.red,), flex: 1,),
+          Flexible(child: Container(color: Colors.orange,), flex: 6,),
+          Flexible(child: Container(color: Colors.green,), flex: 7,),
+          Flexible(child: Container(color: Colors.blue,), flex: 6,)
+        ],
       ),
     );
   }
--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,11 +1,9 @@
 /*
  <Home 탭 구현 페이지>
-
  23.03.25
   - 코드 수정하면서 혹시 팀원들도 알아야 할 내용 있을 시 여기에 적고 날짜 최신화 좀 해줘 얘들아 (이 줄은 지우지 말아줘)
  23.03.30
   - 코스페이지로 이동 버튼은 리스트뷰로 만들어진 코스들을 넣어야하지만, 아직 없어서 임의로 버튼으로 넘어가서 gui 구성 먼저 해놓으려고 만들어놨음
-
 */
 
 import 'package:flutter/material.dart';
@@ -18,91 +16,92 @@ class homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
-      children: [
-        Flexible(
-            child: Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.height,
-              decoration:
+          children: [
+            Flexible(
+                child: Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.height,
+                  decoration:
                   BoxDecoration(border: Border.all(color: Color(0xffC5C2B3))),
-              child: Row(
-                children: [
-                  Flexible(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: double.infinity,
-                        color: Color(0xffF6F3E0),
-                        child: Container(
-                          width: double.infinity,
-                          height: double.infinity,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border: Border.all(color: Color(0xffC5C2B3))),
-                          margin: EdgeInsets.fromLTRB(60, 25, 0, 25),
+                  child: Row(
+                    children: [
+                      Flexible(
                           child: Container(
-                            width: double.infinity,
+                            width: MediaQuery.of(context).size.width,
                             height: double.infinity,
-                            margin: EdgeInsets.fromLTRB(7, 10, 10, 0),
-                            child: Image(
-                              image: AssetImage('images/user_icon.png'),
+                            color: Color(0xffF6F3E0),
+                            child: Container(
+                              width: double.infinity,
+                              height: double.infinity,
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(color: Color(0xffC5C2B3))),
+                              margin: EdgeInsets.fromLTRB(60, 25, 0, 25),
+                              child: Container(
+                                width: double.infinity,
+                                height: double.infinity,
+                                margin: EdgeInsets.fromLTRB(7, 10, 10, 0),
+                                child: Image(
+                                  image: AssetImage('images/user_icon.png'),
+                                ),
+                              ),
                             ),
                           ),
-                        ),
-                      ),
-                      flex: 4),
-                  Flexible(
-                      child: Container(
-                        width: MediaQuery.of(context).size.width,
-                        height: double.infinity,
-                        color: Color(0xffF6F3E0),
-                        child: Container(
-                          color: Color(0xffF6F3E0),
-                          margin: EdgeInsets.fromLTRB(30, 30, 50, 30),
-                          child: Column(
-                            children: [
-                              Flexible(
-                                  child: Container(
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      alignment: Alignment.center,
-                                      child: Text('최민성 님')),
-                                  flex: 5),
-                              Flexible(
-                                  child: Container(
-                                      width: double.infinity,
-                                      height: double.infinity,
-                                      alignment: Alignment.center,
-                                      child: Text('270 개')),
-                                  flex: 5)
-                            ],
+                          flex: 4),
+                      Flexible(
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            height: double.infinity,
+                            color: Color(0xffF6F3E0),
+                            child: Container(
+                              color: Color(0xffF6F3E0),
+                              margin: EdgeInsets.fromLTRB(30, 30, 50, 30),
+                              child: Column(
+                                children: [
+                                  Flexible(
+                                      child: Container(
+                                          width: double.infinity,
+                                          height: double.infinity,
+                                          alignment: Alignment.center,
+                                          child: Text('최민성 님')),
+                                      flex: 5),
+                                  Flexible(
+                                      child: Container(
+                                          width: double.infinity,
+                                          height: double.infinity,
+                                          alignment: Alignment.center,
+                                          child: Text('270 개')),
+                                      flex: 5)
+                                ],
+                              ),
+                            ),
                           ),
-                        ),
-                      ),
-                      flex: 6)
-                ],
-              ),
-            ),
-            flex: 3),
-        Flexible(
-            child: Container(
-                width: double.infinity,
-                height: MediaQuery.of(context).size.height,
-                decoration:
+                          flex: 6)
+                    ],
+                  ),
+                ),
+                flex: 3),
+            Flexible(
+                child: Container(
+                    width: double.infinity,
+                    height: MediaQuery.of(context).size.height,
+                    decoration:
                     BoxDecoration(border: Border.all(color: Color(0xffC5C2B3))),
-                child: Text('여기에 리스트뷰로 코스들 넣기')),
-            flex: 7),
-        //버튼은 코스 페이지 gui 확인하기 위해서 만든 버튼임
-        Flexible(
-            child: ElevatedButton(
-              child: Text('코스페이지로 이동'),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                MaterialPageRoute(builder: (context) => coursepage()),
-                );
-              },
-            ))
-      ],
-    ));
+                    child: Text('여기에 리스트뷰로 코스들 넣기')),
+                flex: 5),
+            //버튼은 코스 페이지 gui 확인하기 위해서 만든 버튼임
+            Flexible(
+                child: ElevatedButton(
+                  child: Text('코스페이지로 이동'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => coursepage()),
+                    );
+                  },
+                ),
+            flex: 2,)
+          ],
+        ));
   }
 }
