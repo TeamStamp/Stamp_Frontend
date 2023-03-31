55eb212d165ef79747ee6defd430d83a94acb58c
201904027 <scottmctominay39@naver.com>
2023-03-31 14:06:59 +0900

    23.03.31 course_page.dart 수정

--- lib/course_page.dart
+++ lib/course_page.dart
@@ -1,6 +1,6 @@
 /*
   23.03.30
-    - 바텀 네비게이션 바를 유지하고 넘어가야 하는데 모르겠음 (도와줘요 영훈몬)
+    -
 
 
 
@@ -42,21 +42,43 @@ class _coursepageState extends State<coursepage> {
       ),
       body: Column(
         children: [
+          // 앱바 바로 밑에 위치한 카카오지하철 느낌의 로고
           Flexible(
               child: Container(
+                margin: EdgeInsets.fromLTRB(0, 10, 0, 10),
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
-                color: Colors.red,
-                child: Text('카카오지하철 느낌의 이미지 넣는 구역'),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    fit: BoxFit.cover,
+                    image: AssetImage('images/course_page.image01.png')
+                  )
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text('수원시 영통구', textAlign: TextAlign.center, style: TextStyle(fontSize: 12, color: Color(0xffCDAD5C)),),
+                  ],
+                ),
               ),
               flex: 2,
           ),
           Flexible(
               child: Container(
+                margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
-                color: Colors.orange,
-                child: Text('선택한 코스를 이루는 장소들 사진 나열하는 구역'),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(30),
+                  border: Border.all(color: Color(0xffCDAD5C), width: 2)
+                ),
+                child: Container(
+                  width: double.infinity,
+                  height: double.infinity,
+                  color: Colors.blueGrey,
+                  margin: EdgeInsets.fromLTRB(0, 15, 0, 15),
+                  child: Text('띠 모양은 배경이미지로 넣고 그 위에 장소별 이미지 Row로 정렬'),
+                ),
               ),
               flex: 5
           ),
@@ -64,16 +86,19 @@ class _coursepageState extends State<coursepage> {
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
-                color: Colors.green,
-                child: Text('각 장소별 상세 설명'),
+                color: Color(0xffFDFAF4),
+                child: Text('1. 컨테이너 안에 리스트뷰 넣기 \n'
+                            '2. 리스트뷰 안에 Column 넣기 \n'
+                            '3. Coulmn 안에 여러개의 컨테이너 넣기 \n'
+                            '4. 각 컨테이너 안에 장소별 상세 정보 넣기'),
               ),
-              flex: 7
+              flex: 8
           ),
           Flexible(
               child: Container(
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height,
-                color: Colors.blue,
+                color: Color(0xffFDFAF4),
                 child: Text('댓글 리스트 형식으로 넣는 구역'),
               ),
               flex: 6
