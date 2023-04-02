db80a84c1d30289fcd882320ac4139b52e1563e4
Euijung_Kim <dmlwjd5161@gmail.com>
2023-04-03 03:28:13 +0900

    리스트뷰 - 코스리스트 추가

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -13,6 +13,99 @@ class homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ListViewPage(),
+    );
+  }
+}
+
+class ListViewPage extends StatefulWidget {
+  const ListViewPage({Key? key}) : super(key: key);
+
+  @override
+  State<ListViewPage> createState() => _ListViewPageState();
+}
+
+class _ListViewPageState extends State<ListViewPage> {
+  var titleList = [
+    '강남대학교 코스',
+    '청계천 코스',
+    '전주 한옥마을 코스',
+    '여수밤바다 코스',
+    '거제도 해변 코스',
+    '대전 엑스포 코스'
+  ];
+
+  var imageList = [
+    'images/place1',
+    'images/place2',
+    'images/place3',
+    'images/place4',
+    'images/place5',
+    'images/place6'
+  ];
+
+  var description = [
+    '- - - -',
+    '- - - -',
+    '- - - -',
+    '- - - -',
+    '- - - -',
+    '- - - -'
+  ];
+
+  void showPopup(context, title, image, description) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return Dialog(
+          child: Container(
+            width: MediaQuery.of(context).size.width * 0.7,
+            height: 380,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10), color: Colors.white),
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    image,
+                    width: 200,
+                    height: 200,
+                  ),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  title,
+                  style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.black87),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8),
+                  child: Text(
+                    description,
+                    maxLines: 3,
+                    style: TextStyle(
+                      fontSize: 15,
+                      color: Color(0xffC5C2B3),
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
         body: Column(
           children: [
@@ -82,12 +175,48 @@ class homepage extends StatelessWidget {
                 flex: 3),
             Flexible(
                 child: Container(
-                    width: double.infinity,
-                    height: MediaQuery.of(context).size.height,
-                    decoration:
-                    BoxDecoration(border: Border.all(color: Color(0xffC5C2B3))),
-                    child: Text('여기에 리스트뷰로 코스들 넣기')),
-                flex: 5),
+                  child: ListView.builder(
+                  itemCount: titleList.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        print(titleList[index]);
+                        showPopup(context, titleList[index], imageList[index],
+                            description[index]);
+                      },
+                      child: Card(
+                        child: Row(
+                          children: [
+                            SizedBox(
+                              width: 100,
+                              height: 100,
+                              child: Image.asset(imageList[index]),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(10),
+                              child: Column(
+                                children: [
+                                  Text(
+                                    titleList[index],
+                                    style: TextStyle(
+                                        fontSize: 22,
+                                        fontWeight: FontWeight.bold,
+                                        color: Colors.black87),
+                                  ),
+                                  const SizedBox(
+                                    height: 10,
+                                  )
+                                ],
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                ),
+                )
+            ),
             //버튼은 코스 페이지 gui 확인하기 위해서 만든 버튼임
             Flexible(
                 child: ElevatedButton(
